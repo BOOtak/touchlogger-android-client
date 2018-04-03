2b03323a3f43ab7ebf446b733092ac8aa458c40f
Kirill Leyfer <kolbasisha@gmail.com>
2018-04-03 23:58:19 +0300

    Remove FloatingIndicationService.

--- app/src/main/java/org/leyfer/thesis/touchlogger_dirty/activity/MainActivity.java
+++ app/src/main/java/org/leyfer/thesis/touchlogger_dirty/activity/MainActivity.java
@@ -1,19 +1,13 @@
 package org.leyfer.thesis.touchlogger_dirty.activity;
 
 import android.Manifest;
-import android.app.AlertDialog;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -23,7 +17,6 @@ import android.widget.TextView;
 import org.leyfer.thesis.touchlogger_dirty.R;
 import org.leyfer.thesis.touchlogger_dirty.dialog.ErrorAlertDialog;
 import org.leyfer.thesis.touchlogger_dirty.handler.InjectProgressHandler;
-import org.leyfer.thesis.touchlogger_dirty.service.FloatingIndicationService;
 import org.leyfer.thesis.touchlogger_dirty.utils.Config;
 import org.leyfer.thesis.touchlogger_dirty.utils.JniApi;
 
@@ -34,7 +27,6 @@ import static org.leyfer.thesis.touchlogger_dirty.utils.file.FileUtils.unpackAss
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "TouchLoggeer-dirty";
     private static final int PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
-    private static final int PERMISSION_REQUEST_SYSTEM_ALERT_WINDOW = 2;
     private static final String EXTRA_STARTED_BY_PAYLOAD = "org.leyfer.thesis.extra.started_by_payload";
     private Handler mHandler;
 
@@ -44,8 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (!unpackAssets()) {
             Log.e(TAG, "Unable to unpack assets!");
-            //TODO: finish main activity on dismiss / cancel
-            new ErrorAlertDialog(this, "Unable to unpack assets!").show();
+            showErrorDialog("Unable to unpack assets!");
         }
 
         setContentView(R.layout.activity_main);
@@ -58,16 +49,6 @@ public class MainActivity extends AppCompatActivity {
             startedByPayload = extras.getBoolean(EXTRA_STARTED_BY_PAYLOAD, false);
             if (startedByPayload) {
                 tv.setText(R.string.started_by_payload);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (!Settings.canDrawOverlays(this)) {
-                        requestOverlayWindow();
-                    } else {
-                        showFloatingIndicator();
-                    }
-                } else {
-                    showFloatingIndicator();
-                }
             } else {
                 tv.setText(JniApi.stringFromJNI());
             }
@@ -103,11 +84,6 @@ public class MainActivity extends AppCompatActivity {
         checkSDCardPermission();
     }
 
-    private void showFloatingIndicator() {
-        Intent floatingIndicatorIntent = new Intent(MainActivity.this, FloatingIndicationService.class);
-        startService(floatingIndicatorIntent);
-    }
-
     private boolean unpackAssets() {
         String abi;
         if (android.os.Build.VERSION.SDK_INT
@@ -132,29 +108,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void checkSDCardPermission() {
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(MainActivity.this,
                     new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     PERMISSION_REQUEST_WRITE_EXTERNAL_STORAGE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case PERMISSION_REQUEST_SYSTEM_ALERT_WINDOW:
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    if (!Settings.canDrawOverlays(this)) {
-                        showErrorDialog("Unable to show overlay with coordinates!");
-                    } else {
-                        showFloatingIndicator();
-                    }
-                }
-            default:
-                break;
         }
     }
 
@@ -199,31 +159,5 @@ public class MainActivity extends AppCompatActivity {
         });
 
         errorAlertDialog.show();
-    }
-
-    private void requestOverlayWindow() {
-        new AlertDialog.Builder(this)
-                .setMessage("Please allow system alert dialogs to show coordinates window!")
-                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                    @RequiresApi(api = Build.VERSION_CODES.M)
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                                Uri.parse("package:" + getPackageName()));
-                        startActivityForResult(intent, PERMISSION_REQUEST_SYSTEM_ALERT_WINDOW);
-                    }
-                })
-                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        showErrorDialog("Unable to show overlay with coordinates!");
-                    }
-                })
-                .setOnCancelListener(new DialogInterface.OnCancelListener() {
-                    @Override
-                    public void onCancel(DialogInterface dialog) {
-                        showErrorDialog("Unable to show overlay with coordinates!");
-                    }
-                }).create().show();
     }
 }
